06b82203a870a709b99c59bcd0c95c81eb4b817c
mgriffioen <mgriffioen@gmail.com>
2026-08-12 10:37:57 -0400

    Update words.js

--- server/words.js
+++ server/words.js
@@ -6,7 +6,7 @@ const WORDS = [
   'lake', 'fireworks', 'beer', 'raft', 'slide', 'license plate', 'shark', 
   'hot dog', 'ribs', 'kubb', 'waterworld', 'the water', 'shangri-la', 'omp', 
   'dune', 'spiral', 'nice', 'bocce', 'erik', 'benedict', 'mark', 'sean', 
-  'carl', 'dusty', 'paul', 'brandon', 'cigarrette',
+  'carl', 'dusty', 'paul', 'brandon', 'cigarette',
 
   // Everyday objects
   'umbrella', 'toothbrush', 'backpack', 'candle', 'mirror', 'blanket', 'ladder',
